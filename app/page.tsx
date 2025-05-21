1fed9c16e6437eee08380c9f1310b0b8040b90da
Gildácio Lopes <gildacioj399@gmail.com>
2025-05-21 08:49:06 -0300

    feat: add header and init Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+import { Input } from "./_components/ui/input"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Button</Button>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        {/* TEXTO */}
+        <h2 className="text-xl font-bold">Olá, Gildácio!</h2>
+        <p>Quarta-feira, 21 de Maio.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Pesquisar" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.svg"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
